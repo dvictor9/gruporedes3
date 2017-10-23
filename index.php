faa7e2f021478d18bccafc435c308460e5df8f60
dvictor9 <32970531+dvictor9@users.noreply.github.com>
2017-10-23 08:22:12 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Estamos aprendendo PHP!</h1>
 <?php
+ $texto = "Olá, \n vamor testar este comando pra ver. \n\n Att. \n Leonardo Luiz.\n";
+echo $texto;
 echo "GRUPO 3\n \r";
 echo "REDES DE COMPUTADORES - ESTÁCIO DE SA - FUNDAMENTOS DE SI /n \r";
 echo "DANIEL VICTOR \n \r";
+echo "</br>";
 echo "DOUGLAS HENRIQUE \n \r";
 echo "FABIANO RODRIGUES \n \r";
 echo "GLAUCO LUIZ \n \r";
+echo "</br>";
 echo "MARCELO FELIPE \n \r";
 echo "E NOIS! \n";
 ?>
+ </body>
+</html>
