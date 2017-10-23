edb24e715d54b0cb867de387db502b96dccfaadc
dvictor9 <32970531+dvictor9@users.noreply.github.com>
2017-10-23 08:29:08 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <html>
   <body>
-    <h1>Estamos aprendendo PHP!</h1>
+    <h1>Serviço web em nuvem</h1>
 <?php
- $texto = "Olá, \n vamor testar este comando pra ver. \n\n Att. \n Leonardo Luiz.\n";
 echo $texto;
 echo "GRUPO 3\n \r";
-echo "REDES DE COMPUTADORES - ESTÁCIO DE SA - FUNDAMENTOS DE SI /n \r";
+echo "</br>";
+echo "REDES DE COMPUTADORES - ESTÁCIO DE SA - FUNDAMENTOS DE SI \n \r";
+echo "</br>";
 echo "DANIEL VICTOR \n \r";
 echo "</br>";
 echo "DOUGLAS HENRIQUE \n \r";
+echo "</br>";
 echo "FABIANO RODRIGUES \n \r";
+echo "</br>";
 echo "GLAUCO LUIZ \n \r";
 echo "</br>";
 echo "MARCELO FELIPE \n \r";
-echo "E NOIS! \n";
+echo "</br>";
+echo "OBRIGADO! \n";
 ?>
  </body>
 </html>
